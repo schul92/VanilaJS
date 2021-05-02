ecb872ba0da03ecd536cf1ac4c0933bd2fba23f0
Steve Song <steve.b.song92@gmail.com>
2021-05-01 22:03:21 -0400

    js files added and css applied

---
+++ clock.js
@@ -0,0 +1,36 @@
+
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
+
+const hourHand = document.querySelector('.hand-hour');
+const minuteHand = document.querySelector('.hand-minute');
+const secondHand = document.querySelector('.hand-seconds');
+
+function getTime() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegree = (((seconds / 60) * 360) + 90);
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`
+
+    const minutes = now.getMinutes();
+    const minutesDegree = (((minutes / 60) * 360) + 90);
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+
+
+    const hours = now.getHours();
+    console.log(hours)
+    const hoursDegree = (((hours / 60) * 360) + 90) + 15*10;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`
+
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+}
+
+function init() {
+getTime();
+setInterval(getTime, 1000);
+}
+
+init();
